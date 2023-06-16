b57caf10b2263d90d13f078774fefca21a922831
Florian Scherf <mail@florianscherf.de>
2023-06-16 20:54:11 +0200

    installer: make routes reusable
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- lona_picocss/__init__.py
+++ lona_picocss/__init__.py
@@ -1,25 +1,11 @@
 import logging
 
-from lona import Route
-
-from lona_picocss.views.components.typography import TypographyView
-from lona_picocss.views.components.scroller import ScrollerView
-from lona_picocss.views.components.progress import ProgressView
-from lona_picocss.views.components.buttons import ButtonsView
-from lona_picocss.views.components.cards import CardsView
-from lona_picocss.views.components.icons import IconsView
-from lona_picocss.views.components.forms import FormsView
-from lona_picocss.views.components.modal import ModalView
-from lona_picocss.views.components.tabs import TabsView
-from lona_picocss.views.settings import SettingsView
-from lona_picocss.views.it_works import ItWorksView
+from lona_picocss.routes import IT_WORKS_ROUTE, SETTINGS_ROUTE, DEMO_ROUTES
 from lona_picocss.middlewares import LonaPicocssMiddleware
 from lona_picocss.navigation import get_debug_navigation, NavItem
 from lona_picocss import settings
 
 from lona_picocss.views.error_views import (
-    ForbiddenErrorView,
-    InternalErrorView,
     Error403View,
     Error404View,
     Error500View,
@@ -59,82 +45,12 @@ def install_picocss(app, debug=False):
     app.middleware(LonaPicocssMiddleware)
 
     # setup views
-    app.routes.append(
-        Route(
-            '/_picocss/it-works(/)',
-            ItWorksView,
-            name='picocss__it_works',
-        ),
-    )
+    app.routes.append(IT_WORKS_ROUTE)
 
     if debug:
         app.routes.extend([
-
-            # settings
-            Route(
-                '/_picocss/settings(/)',
-                SettingsView,
-                name='picocss__settings',
-            ),
-
-            # error views
-            Route(
-                '/_picocss/internal-error(/)',
-                InternalErrorView,
-                name='picocss__internal_error',
-            ),
-            Route(
-                '/_picocss/forbidden-error(/)',
-                ForbiddenErrorView,
-                name='picocss__forbidden_error',
-            ),
-
-            # component views
-            Route(
-                '/_picocss/components/typography(/)',
-                TypographyView,
-                name='picocss__components__typography',
-            ),
-            Route(
-                '/_picocss/components/icons(/)',
-                IconsView,
-                name='picocss__components__icons',
-            ),
-            Route(
-                '/_picocss/components/cards(/)',
-                CardsView,
-                name='picocss__components__cards',
-            ),
-            Route(
-                '/_picocss/components/forms(/)',
-                FormsView,
-                name='picocss__components__forms',
-            ),
-            Route(
-                '/_picocss/components/buttons(/)',
-                ButtonsView,
-                name='picocss__components__buttons',
-            ),
-            Route(
-                '/_picocss/components/progress(/)',
-                ProgressView,
-                name='picocss__components__progress',
-            ),
-            Route(
-                '/_picocss/components/tabs(/)',
-                TabsView,
-                name='picocss__components__tabs',
-            ),
-            Route(
-                '/_picocss/components/modal(/)',
-                ModalView,
-                name='picocss__components__modal',
-            ),
-            Route(
-                '/_picocss/components/scroller(/)',
-                ScrollerView,
-                name='picocss__components__scroller',
-            ),
+            SETTINGS_ROUTE,
+            *DEMO_ROUTES,
         ])
 
         if 'PICOCSS_NAVIGATION' not in app.settings:
