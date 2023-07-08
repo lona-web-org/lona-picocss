624e3aac1ba950d523305f93ca6a7f12c6790677
Florian Scherf <mail@florianscherf.de>
2023-07-08 21:39:12 +0200

    django: error views: move django import into views
    Previously, `django.shortcuts.render` was imported on module level which caused
    this import error when Django was not installed:
    
        Traceback (most recent call last):
          File "/home/fscherf/devel/lona-picocss/test-script/test_script.py", line 3, in <module>
            from lona_picocss import install_picocss
          File "/home/fscherf/devel/lona-picocss/lona_picocss/__init__.py", line 3, in <module>
            from lona_picocss.routes import IT_WORKS_ROUTE, SETTINGS_ROUTE, DEMO_ROUTES
          File "/home/fscherf/devel/lona-picocss/lona_picocss/routes.py", line 1, in <module>
            from lona_picocss.views.components.typography import TypographyView
          File "/home/fscherf/devel/lona-picocss/lona_picocss/views/__init__.py", line 1, in <module>
            from .django_error_views import handler403, handler404, handler500  # NOQA
            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
          File "/home/fscherf/devel/lona-picocss/lona_picocss/views/django_error_views.py", line 3, in <module>
            from django.shortcuts import render
        ModuleNotFoundError: No module named 'django'
    
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- lona_picocss/views/django_error_views.py
+++ lona_picocss/views/django_error_views.py
@@ -1,21 +1,25 @@
 import sys
 
-from django.shortcuts import render
-
 
 def handler403(request, exception=None):
+    from django.shortcuts import render
+
     return render(request, 'picocss/django/403.html', {
         'exception': exception,
     }, status=403)
 
 
 def handler404(request, exception=None):
+    from django.shortcuts import render
+
     return render(request, 'picocss/django/404.html', {
         'exception': exception,
     }, status=404)
 
 
 def handler500(request, exception=None):
+    from django.shortcuts import render
+
     exception = exception or sys.exc_info()[1]
 
     return render(request, 'picocss/django/500.html', {
